0a8d12145381b208e48df6db67d6fd999ee8d155
René Post <github@xpt.nl>
2019-05-13 23:02:38 +0200

    Move jig for SwitchMap to another location in the same file.

--- transforming.go
+++ transforming.go
@@ -18,17 +18,6 @@ func (o ObservableFoo) MapBar(project func(foo) bar) ObservableBar {
 	return observable
 }
 
-//jig:template Observable<Foo> SwitchMap<Bar>
-
-// SwitchMapBar transforms the items emitted by an ObservableFoo by applying a
-// function to each item an returning an ObservableBar. In doing so, it behaves much like
-// MergeMap (previously FlatMap), except that whenever a new ObservableBar is emitted
-// SwitchMap will unsubscribe from the previous ObservableBar and begin emitting items
-// from the newly emitted one.
-func (o ObservableFoo) SwitchMapBar(project func(foo) ObservableBar) ObservableBar {
-	return o.MapObservableBar(project).SwitchAll()
-}
-
 //jig:template Observable<Foo> MergeMap<Bar>
 
 // MergeMapBar transforms the items emitted by an ObservableFoo by applying a
@@ -58,4 +47,15 @@ func (o ObservableFoo) ScanBar(accumulator func(bar, foo) bar, seed bar) Observa
 		o(observer, subscribeOn, subscriber)
 	}
 	return observable
+}
+
+//jig:template Observable<Foo> SwitchMap<Bar>
+
+// SwitchMapBar transforms the items emitted by an ObservableFoo by applying a
+// function to each item an returning an ObservableBar. In doing so, it behaves much like
+// MergeMap (previously FlatMap), except that whenever a new ObservableBar is emitted
+// SwitchMap will unsubscribe from the previous ObservableBar and begin emitting items
+// from the newly emitted one.
+func (o ObservableFoo) SwitchMapBar(project func(foo) ObservableBar) ObservableBar {
+	return o.MapObservableBar(project).SwitchAll()
 }
