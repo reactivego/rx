46be9b47d6a3885c6895324b9f490780f2913b67
René Post <github@xpt.nl>
2019-05-29 09:18:29 +0200

    Update documentation for ObserveFunc.

--- generic/observable.go
+++ generic/observable.go
@@ -2,9 +2,13 @@ package rx
 
 //jig:template <Foo>ObserveFunc
 
-// FooObserveFunc is essentially the observer, a function that gets called
-// whenever the observable has something to report.
-type FooObserveFunc func(foo, error, bool)
+// FooObserveFunc is the observer, a function that gets called whenever the
+// observable has something to report. The next argument is the item value that
+// is only valid when the done argument is false. When done is true and the err
+// argument is not nil, then the observable has terminated with an error.
+// When done is true and the err argument is nil, then the observable has
+// completed normally.
+type FooObserveFunc func(next foo, err error, done bool)
 
 var zeroFoo foo
 
