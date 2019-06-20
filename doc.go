721d5500b47c973fb073c88895d3f26db1094b1f
René Post <github@xpt.nl>
2019-06-20 15:45:32 +0200

    Update comments.

--- doc.go
+++ doc.go
@@ -1,59 +1,61 @@
-// Package rx offers Reactive Extensions (ReactiveX) for Go, an API for
-// asynchronous programming with observable streams.
-//
-// The coherent set of functions and types offered by this package make it
-// easier to develop event-based and asynchronous programs using observable
-// streams (Observables).
-//
-// Heterogeneous vs typesafe
-// 
-// A sequence or stream can be either:
-//	typesafe, guaranteed to contain items of a single type. e.g. string.
-//	heterogeneous, contain items of any type.
-//
-// The implementation in this package is rx/generic specialized on interface{}.
-// As a value of interface{} accepts any type, this makes this package heterogeneous.
-//
-// By using Generics for Go (https://github.com/reactivego/jig) it is also
-// possible to have the jig command generate typesafe implementations.
-//
-// Alias any
-//
-// In this package we have defined a non-exported alias 'any' for 'interface{}'.
-// So in the examples, instead of 'interface{}' you will find 'any' being used.
-// If you choose to use this package in your own code, you can decide for yourself
-// how to call the interface{} type, this is not enforced by this package.
-//
-// Creating Observables
-//
-// Functions to create new observables from lists, slices and channels.
-//	From          http://reactivex.io/documentation/operators/from.html
-//	FromChan
-//	FromSlice
-//
-// Combining Observables
-//
-// Functions and methods on observable that allow combining multiple observables.
-// 	Concat        http://reactivex.io/documentation/operators/concat.html
-//
-// Transforming Observables
-//
-// Functions and methods to transform observables to same or other types.
-// 	Filter        http://reactivex.io/documentation/operators/filter.html
-// 	MergeMap      http://reactivex.io/documentation/operators/flatmap.html
-// 	Map           http://reactivex.io/documentation/operators/map.html
-// 	Scan          http://reactivex.io/documentation/operators/scan.html
-//
-// Side Effects triggered by Observables
-//
-// Methods on observables to trigger side effects.
-//	Do            http://reactivex.io/documentation/operators/do.html
-//	DoOnError
-//	DoOnComplete
-//	Finally
-//
-// For an overview of all implemented operators, see
-// https://github.com/reactivego/rx
+/*
+Package rx provides Reactive Extensions (ReactiveX) for Go, an API for
+asynchronous programming with observable streams.
+
+The coherent set of functions and types offered by this package make it
+easier to develop event-based and asynchronous programs using observable
+streams (Observables).
+
+Heterogeneous vs typesafe
+
+A sequence or stream can be either:
+	typesafe, guaranteed to contain items of a single type. e.g. string.
+	heterogeneous, contain items of any type.
+
+The implementation in this package is rx/generic specialized on interface{}.
+As a value of interface{} accepts any type, this makes this package heterogeneous.
+
+By using Generics for Go (https://github.com/reactivego/jig) it is also
+possible to have the jig command generate typesafe implementations.
+
+Alias any
+
+In this package we have defined a non-exported alias 'any' for 'interface{}'.
+So in the examples, instead of 'interface{}' you will find 'any' being used.
+If you choose to use this package in your own code, you can decide for yourself
+how to call the interface{} type, this is not enforced by this package.
+
+Creating Observables
+
+Functions to create new observables from lists, slices and channels.
+	From          http://reactivex.io/documentation/operators/from.html
+	FromChan
+	FromSlice
+
+Combining Observables
+
+Functions and methods on observable that allow combining multiple observables.
+	Concat        http://reactivex.io/documentation/operators/concat.html
+
+Transforming Observables
+
+Functions and methods to transform observables to same or other types.
+	Filter        http://reactivex.io/documentation/operators/filter.html
+	MergeMap      http://reactivex.io/documentation/operators/flatmap.html
+	Map           http://reactivex.io/documentation/operators/map.html
+	Scan          http://reactivex.io/documentation/operators/scan.html
+
+Side Effects triggered by Observables
+
+Methods on observables to trigger side effects.
+	Do            http://reactivex.io/documentation/operators/do.html
+	DoOnError
+	DoOnComplete
+	Finally
+
+For an overview of all implemented operators, see
+https://github.com/reactivego/rx
+*/
 package rx
 
 import _ "github.com/reactivego/rx/generic"
--- generic/doc.go
+++ generic/doc.go
@@ -1,20 +1,22 @@
-// Package rx offers generic Reactive Extensions (ReactiveX) for Go.
-// It's a generics library for composing asynchronous and event-based programs
-// using observable sequences. The library consists of more than a 100 generic 
-// templates to enable type-safe programming with observable streams.
-// To use it, you will need Generics for Go (https://github.com/reactivego/jig).
-//
-// Using the library is very simple. Import the library with the blank
-// identifier `_` as the package name:
-//	import _ "github.com/reactivego/rx"
-// The side effect of this import is that
-// generics from the library can now be accessed by the jig tool. Then start
-// using generics from the library and run jig to generate code. Take a look at
-// the Quick Start guide to see how it all fits together
-// https://github.com/ReactiveGo/rx/blob/master/doc/QUICKSTART.md.
-//
-// For an overview of all implemented operators, see
-// https://github.com/reactivego/rx
+/*
+Package rx provides generic Reactive Extensions (ReactiveX) for Go.
+It's a generics library for composing asynchronous and event-based programs
+using observable sequences. The library consists of more than a 100 generic 
+templates to enable type-safe programming with observable streams.
+To use it, you will need Generics for Go (https://github.com/reactivego/jig).
+
+Using the library is very simple. Import the library with the blank
+identifier `_` as the package name:
+	import _ "github.com/reactivego/rx"
+The side effect of this import is that
+generics from the library can now be accessed by the jig tool. Then start
+using generics from the library and run jig to generate code. Take a look at
+the Quick Start guide to see how it all fits together
+https://github.com/ReactiveGo/rx/blob/master/doc/QUICKSTART.md.
+
+For an overview of all implemented operators, see
+https://github.com/reactivego/rx
+*/
 package rx
 
 // foo is the first metasyntactic type. Use the jig:type pragma to tell jig that
