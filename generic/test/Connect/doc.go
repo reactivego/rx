6c7f2e6f6bbd574e9a4ee6a844d103ed134c1b1b
René Post <github@xpt.nl>
2019-06-26 19:39:39 +0200

    Add test folder for Connect method on Connectable.

---
+++ generic/test/Connect/doc.go
@@ -0,0 +1,8 @@
+/*
+Method Connect documentation and tests.
+
+	Connects	http://reactivex.io/documentation/operators/connect.html
+*/
+package Connect
+
+import _ "github.com/reactivego/rx/generic"
---
+++ generic/test/Connect/example_test.go
@@ -0,0 +1,61 @@
+package Connect
+
+import (
+	"fmt"
+)
+
+func Example_connect() {
+	// source is an ObservableInt
+	source := FromInts(1, 2)
+
+	// pub is a ConnectableInt
+	pub := source.Publish()
+
+	// scheduler will run a task asynchronously on a new goroutine.
+	scheduler := NewGoroutineScheduler()
+
+	// First subscriber (asynchronous)
+	sub1 := pub.SubscribeOn(scheduler).
+		MapString(func(v int) string {
+			return fmt.Sprintf("value is %d", v)
+		}).
+		Subscribe(func(next string, err error, done bool) {
+			if !done {
+				fmt.Println("sub1", next)
+			}
+		})
+
+	// Second subscriber (asynchronous)
+	sub2 := pub.SubscribeOn(scheduler).
+		MapBool(func(v int) bool {
+			return v > 1
+		}).
+		Subscribe(func(next bool, err error, done bool) {
+			if !done {
+				fmt.Println("sub2", next)
+			}
+		})
+
+	// Third subscriber (asynchronous)
+	sub3 := pub.SubscribeOn(scheduler).
+		Subscribe(func(next int, err error, done bool) {
+			if !done {
+				fmt.Println("sub3", next)
+			}
+		})
+
+	// Connect will cause the publisher to subscribe to the source
+	pub.Connect()
+
+	sub1.Wait()
+	sub2.Wait()
+	sub3.Wait()
+
+	// Unordered output:
+	// sub1 value is 1
+	// sub2 false
+	// sub3 1
+	// sub2 true
+	// sub3 2
+	// sub1 value is 2
+}
