c6e3ba73d089949deb54eb185efebbcab159d41f
René Post <github@xpt.nl>
2019-06-27 10:07:28 +0200

    Don't test Observer in isolation, merge into Observable test.

--- generic/test/Observable/example_test.go
+++ generic/test/Observable/example_test.go
@@ -31,3 +31,29 @@ func Example_observable() {
 	// error
 	// complete
 }
+
+
+// An ObserveFunc has Next, Error and Complete methods defined. But it is just
+// a callback function meant to be passed to an observable.
+func Example_observeFunc() {
+	var observe ObserveFunc
+	observe = func(next interface{}, err error, done bool) {
+		switch {
+		case !done:
+			fmt.Println(next)
+		case err != nil:
+			fmt.Println(err)
+		default:
+			fmt.Println("complete")
+		}
+	}
+
+	observe.Next(123)
+	observe.Error(RxError("error"))
+	observe.Complete()
+
+	// Output:
+	// 123
+	// error
+	// complete
+}
--- generic/test/Observer/doc.go
+++
@@ -1,9 +0,0 @@
-/*
-Observer documentation and tests.
-
-	Observer 	http://reactivex.io/documentation/observable.html
-
-*/
-package Observer
-
-import _ "github.com/reactivego/rx"
