0105d19b43206e7b40c3649ec6c20303fdd44081
René Post <github@xpt.nl>
2019-07-01 10:00:06 +0200

    Rewrite Observable example to no longer use ObserveFunc methods.

--- generic/test/Observable/example_test.go
+++ generic/test/Observable/example_test.go
@@ -4,37 +4,8 @@ import (
 	"fmt"
 )
 
-// An observable is just a function, that takes an observe function, a scheduler,
-// and a subscriber.
-func Example_observable() {
-	observable := func(observe ObserveFunc, s Scheduler, u Subscriber) {
-		observe.Next(123)
-		observe.Error(RxError("error"))
-		observe.Complete()
-	}
-
-	observe := func(next interface{}, err error, done bool) {
-		switch {
-		case !done:
-			fmt.Println(next)
-		case err != nil:
-			fmt.Println(err)
-		default:
-			fmt.Println("complete")
-		}
-	}
-
-	observable(observe, nil, nil)
-
-	// Output:
-	// 123
-	// error
-	// complete
-}
-
-
-// An ObserveFunc has Next, Error and Complete methods defined. But it is just
-// a callback function meant to be passed to an observable.
+// An ObserveFunc is just a callback function meant to be passed to an
+// observable.
 func Example_observeFunc() {
 	var observe ObserveFunc
 	observe = func(next interface{}, err error, done bool) {
@@ -48,9 +19,45 @@ func Example_observeFunc() {
 		}
 	}
 
-	observe.Next(123)
-	observe.Error(RxError("error"))
-	observe.Complete()
+	// Next
+	observe(123, nil, false)
+	// Error
+	observe(zero, RxError("error"), true)
+	// Complete
+	observe(zero, nil, true)
+
+	// Output:
+	// 123
+	// error
+	// complete
+}
+
+// An observable is just a function, that takes an observe function, a scheduler,
+// and a subscriber.
+func Example_observable() {
+	var observable Observable
+	observable = func(observe ObserveFunc, s Scheduler, u Subscriber) {
+		// Next
+		observe(123, nil, false)
+		// Error
+		observe(zero, RxError("error"), true)
+		// Complete
+		observe(zero, nil, true)
+	}
+
+	var observe ObserveFunc
+	observe = func(next interface{}, err error, done bool) {
+		switch {
+		case !done:
+			fmt.Println(next)
+		case err != nil:
+			fmt.Println(err)
+		default:
+			fmt.Println("complete")
+		}
+	}
+
+	observable(observe, nil, nil)
 
 	// Output:
 	// 123
