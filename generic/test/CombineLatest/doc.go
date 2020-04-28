d2a9f446d84fea8f2f5be30cbdc58fbfbe191b36
René Post <github@xpt.nl>
2020-04-28 20:21:37 +0200

    First stab at CombineLatest based on CombineAll.

---
+++ generic/test/CombineLatest/doc.go
@@ -0,0 +1,9 @@
+/*
+	CombineLatest
+
+CombineLatest combines multiple Observables to create an Observable whose
+values are calculated from the latest values of each of its input Observables.
+*/
+package CombineLatest
+
+import _ "github.com/reactivego/rx"
---
+++ generic/test/CombineLatest/example_test.go
@@ -0,0 +1,16 @@
+package CombineLatest
+
+func Example_basic() {
+
+	A := FromInt(1,3,5)
+	B := FromInt(2,4,6)
+
+	CombineLatestInt(A,B).Println()
+
+	// Output:
+	// [1 2]
+	// [3 2]
+	// [3 4]
+	// [5 4]
+	// [5 6]
+}
