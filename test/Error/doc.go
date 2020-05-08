321cad143b49674bb16e894b71c0b8271160ff18
René Post <github@xpt.nl>
2020-05-09 01:17:31 +0200

    Get rid of Error, was an alias for Throw anyway.

--- test/Error/doc.go
+++
@@ -1,9 +0,0 @@
-/*
-Error creates an Observable that emits no items and terminates with an error.
-
-It is an alternative name for Throw.
-
-	Error	http://reactivex.io/documentation/operators/empty-never-throw.html
-*/
-package Error
-
