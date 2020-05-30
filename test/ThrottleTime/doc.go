3cf3ef214f253322d87dde4207d7e56144e8f144
René Post <github@xpt.nl>
2020-05-31 01:19:47 +0200

    Update doc.go for ThrottleTime.

--- test/ThrottleTime/doc.go
+++ test/ThrottleTime/doc.go
@@ -1,5 +1,6 @@
 /*
-ThrottleTime
+ThrottleTime emits when the source emits and then starts a timer during which
+all emissions from the source are ignored. After the timer expires, ThrottleTime
+will again emit the next item the source emits, and so on.
 */
 package ThrottleTime
-
