5f2ecd8145667f529750575736b5332d8fc67961
René Post <github@xpt.nl>
2020-05-31 12:03:07 +0200

    Remove empty links from doc.go.

--- test/Serialize/doc.go
+++ test/Serialize/doc.go
@@ -3,7 +3,5 @@ Serialize forces an observable to make serialized calls and to be well-behaved.
 
 This operator is mostly used internally by other rx library operators that know
 their implementation is violating the observable contract.
-
-	Serialize
 */
 package Serialize
--- test/Single/doc.go
+++ test/Single/doc.go
@@ -3,7 +3,5 @@ Single enforces that the observable sends exactly one data item and then complet
 
 If the observable sends no data before completing or sends more than 1 item before
 completing, this is reported as an error to the observer.
-
-	Single
 */
 package Single
