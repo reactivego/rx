e0ff7ae0ef28ddd2186075dfa07cd95620b62825
René Post <github@xpt.nl>
2020-05-31 20:23:52 +0200

    Fix documentation for SampleTime.

---
+++ test/SampleTime/README.md
@@ -0,0 +1,7 @@
+# SampleTime
+
+[![](../../svg/godev.svg)](https://pkg.go.dev/github.com/reactivego/rx/test/SampleTime?tab=doc)
+[![](../../svg/godoc.svg)](https://godoc.org/github.com/reactivego/rx/test/SampleTime)
+[![](../../svg/rx.svg)](http://reactivex.io/documentation/operators/sample.html)
+
+**SampleTime** emits the most recent item emitted by an Observable within periodic time intervals.
--- test/SampleTime/doc.go
+++ test/SampleTime/doc.go
@@ -1,10 +1,4 @@
 /*
 SampleTime emits the most recent item emitted by an Observable within periodic time intervals.
-
-SampleTime uses a bare goroutine internally in its implementation.
-
-	SampleTime  http://reactivex.io/documentation/operators/sample.html
-	            https://rxjs.dev/api/operators/sampleTime
-	            https://www.learnrxjs.io/learn-rxjs/operators/filtering/sample
 */
 package SampleTime
