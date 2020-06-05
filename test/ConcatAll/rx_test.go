821da10f35ebaf37d79e2b2ed872cde60a29871f
René Post <github@xpt.nl>
2020-06-05 16:19:55 +0200

    Rename BDD to T because this is just TDD with RSpec compatible keywords.

--- test/ConcatAll/rx_test.go
+++ test/ConcatAll/rx_test.go
@@ -22,15 +22,15 @@ func Example_concatAll() {
 	// 3
 }
 
-func TestObservable_ConcatAll(e *testing.T) {
-	Describ(e, "emits", func(t BDD) {
+func TestObservable(e *testing.T) {
+	Describ(e, "ConcatAll", func(t T) {
 		const ms = time.Millisecond
 
-		Contex(t, "with a fast source", func(t BDD) {
+		Contex(t, "with a fast source", func(t T) {
 
-			Contex(t, "and a slow concat", func(t BDD) {
+			Contex(t, "and a slow concat", func(t T) {
 
-				I(t, "should complete before the first emit to observer", func(t BDD) {
+				I(t, "should complete before the first emit to observer", func(t T) {
 					f := func(i int) ObservableInt {
 						return JustInt(i).Delay(10 * ms)
 					}
@@ -50,31 +50,31 @@ func TestObservable_ConcatAll(e *testing.T) {
 					expect := []int{1, 2, 3}
 					actual, err := FromInt(expect...).DoOnComplete(c).MapObservableInt(f).ConcatAll().Do(n).ToSlice()
 
-					Asser(t, NoError(err))
-					Asser(t, Equal(actual, expect))
-					Asser(t, Not(complete.IsZero(), "zero complete time"))
-					Asser(t, Not(next.IsZero(), "zero next time"))
-					Asser(t, Must(complete.Before(next), "complete.Before(next), but complete ", complete.Sub(next), " after next"))
+					Asser(t).NoError(err)
+					Asser(t).Equal(actual, expect)
+					Asser(t).Not(complete.IsZero(), "zero complete time")
+					Asser(t).Not(next.IsZero(), "zero next time")
+					Asser(t).Must(complete.Before(next), "complete.Before(next), but complete ", complete.Sub(next), " after next")
 				})
 
-				I(t, "should emit all concatenated observables", func(t BDD) {
+				I(t, "should emit all concatenated observables", func(t T) {
 					f := func(i int) ObservableInt {
 						return JustInt(i).Delay(10 * ms)
 					}
 					expect := []int{1, 2, 3}
 					actual, err := FromInt(expect...).MapObservableInt(f).ConcatAll().ToSlice()
 
-					Asser(t, NoError(err))
-					Asser(t, Equal(actual, expect))
+					Asser(t).NoError(err)
+					Asser(t).Equal(actual, expect)
 				})
 			})
 		})
 
-		Contex(t, "with a slow source", func(t BDD) {
+		Contex(t, "with a slow source", func(t T) {
 
-			Contex(t, "and a fast concat", func(t BDD) {
+			Contex(t, "and a fast concat", func(t T) {
 
-				I(t, "should start emitting to observer before source has completed", func(t BDD) {
+				I(t, "should start emitting to observer before source has completed", func(t T) {
 					f := func(i int) ObservableInt {
 						return JustInt(i)
 					}
@@ -93,20 +93,20 @@ func TestObservable_ConcatAll(e *testing.T) {
 
 					Interval(10 * ms).Take(3).DoOnComplete(c).MapObservableInt(f).ConcatAll().Do(n).Wait()
 
-					Asser(t, Not(complete.IsZero(), "complete time is zero"))
-					Asser(t, Not(next.IsZero(), "next time is zero"))
-					Asser(t, Must(next.Before(complete), "next.Before(complete) but next ", next.Sub(complete), " after complete"))
+					Asser(t).Not(complete.IsZero(), "complete time is zero")
+					Asser(t).False(next.IsZero(), "next time is zero")
+					Asser(t).Must(next.Before(complete), "next.Before(complete) but next ", next.Sub(complete), " after complete")
 				})
 
-				I(t, "should emit all concatenated observables", func(t BDD) {
+				I(t, "should emit all concatenated observables", func(t T) {
 					f := func(i int) ObservableInt {
 						return JustInt(i)
 					}
 					expect := []int{0, 1, 2}
 					actual, err := Interval(10 * ms).Take(3).MapObservableInt(f).ConcatAll().ToSlice()
 
-					Asser(t, NoError(err))
-					Asser(t, Equal(actual, expect))
+					Asser(t).NoError(err)
+					Asser(t).Equal(actual, expect)
 				})
 			})
 		})
---
+++ test/test.go
@@ -0,0 +1,142 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+)
+
+/*
+T is a struct embedding a *testing.T and therefore has the same methods as a
+testing.T type. It provides support for writing Test Driven Development (TDD)
+specs directly in Go. Is provides a domain-specific language patterned after
+RSpec https://en.wikipedia.org/wiki/RSpec.
+
+Use it as follows:
+
+	func TestObservable_ConcatAll(e *testing.T) {
+	     Describ(e, "emits", func(t T) {
+	         Contex(t, "with a fast source", func(t T) {
+	             Contex(t, "and a slow concat", func(t T) {
+	                 I(t, "should complete before the first emit to observer", func(t T) {
+	                     Asser(t).NoError(nil)
+	                 })
+	             })
+	         })
+	     })
+	}
+*/
+type T struct{ *testing.T }
+
+// Describ(e) is used to describe a data type, method or an example group. This
+// is the outer block which actually contains the test code and it depicts the
+// characteristics of the code enclosed in it.  The name argument is a reference
+// to the module or data type being tested.
+func Describ(e *testing.T, name string, f func(T)) bool {
+	e.Helper()
+	return e.Run(name, func(t *testing.T) { f(T{t}) })
+}
+
+// Contex(t) block is used to describe the context in which the data type or
+// method mentioned in the describe block is being used. The context description
+// argument is used to fence off a specific context in which specific examples
+// are to be performed.
+func Contex(t T, description string, f func(T)) bool {
+	t.Helper()
+	return t.Run(description, func(t *testing.T) { f(T{t}) })
+}
+
+// I(t) describes the specification of the example in the context. The
+// description argument can be considered as the function that the block is
+// expected to perform or in other words it can be considered as a test case.
+func I(t T, description string, f func(T)) bool {
+	t.Helper()
+	return t.Run(description, func(t *testing.T) { f(T{t}) })
+}
+
+// Assert returns an assertion to me made.
+func Asser(t T) Assertion {
+	return Assertion{t.T}
+}
+
+// Assertion represents all assertians that can be made.
+type Assertion struct {
+	*testing.T
+}
+
+// Must asserts that condition is true, when it is false it calls Fatal with:
+//    Must have {args}
+func (a Assertion) Must(condition bool, args ...interface{}) {
+	if !condition {
+		a.Helper()
+		a.Fatal("Must have " + fmt.Sprint(args...))
+	}
+}
+
+// Not asserts that condition is false, when condition is true it calls Fatal with:
+//  Must not have {args}
+func (a Assertion) Not(condition bool, args ...interface{}) {
+	if condition {
+		a.Helper()
+		a.Fatal("Must not have " + fmt.Sprint(args...))
+	}
+}
+
+// True asserts that condition is true, when it is false it calls Fatal with:
+//  Expected {args}
+func (a Assertion) True(condition bool, args ...interface{}) {
+	if !condition {
+		a.Helper()
+		a.Fatal("Expected " + fmt.Sprint(args...))
+	}
+}
+
+// False asserts that condition is false, when condition is true it calls Fatal with:
+//  Unexpected {args}
+func (a Assertion) False(condition bool, args ...interface{}) {
+	if condition {
+		a.Helper()
+		a.Fatal("Unexpected " + fmt.Sprint(args...))
+	}
+}
+
+// Error asserts that err is not nil, but if it is nil, it calls Fatal with:
+//  An error is expected but got nil. {args}
+func (a Assertion) Error(err error, args ...interface{}) {
+	if err == nil {
+		a.Helper()
+		a.Fatal("An error is expected but got nil. " + fmt.Sprint(args...))
+	}
+}
+
+// NoError asserts that err must be nil, when it isn't it calls Fatal with:
+//    No error was expected but got: {err}
+//    {args}
+func (a Assertion) NoError(err error, args ...interface{}) {
+	if err != nil {
+		a.Helper()
+		a.Fatal(fmt.Sprintf("No error was expected but got: %s\n", err.Error()) + fmt.Sprint(args...))
+	}
+}
+
+// Equal assertian compares the type and value of actual and expect and when
+// not equal will call fatal with:
+//	Not equal:
+//	  expect: {expect}
+//	  actual: {actual} {args}
+func (a Assertion) Equal(actual, expect interface{}, args ...interface{}) {
+	a.Helper()
+	tactual := fmt.Sprintf("%T", actual)
+	texpect := fmt.Sprintf("%T", expect)
+	if tactual != texpect {
+		a.Fatal(fmt.Sprintf("Not equal: \n"+
+			"expect: %s\n"+
+			"actual: %s %s", texpect, tactual, fmt.Sprint(args...)))
+	}
+	vactual := fmt.Sprintf("%+v", actual)
+	vexpect := fmt.Sprintf("%+v", expect)
+	if vactual != vexpect {
+		a.Fatal(fmt.Sprintf("Not equal: \n"+
+			"expect: %s\n"+
+			"actual: %s %s", vexpect, vactual, fmt.Sprint(args...)))
+	}
+}
