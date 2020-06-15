ff601cb320a79bf837e33bd567a5979530c3e404
René Post <github@xpt.nl>
2020-06-15 19:46:25 +0200

    Refactor the subject internals

--- generic/subject.go
+++ generic/subject.go
@@ -30,13 +30,18 @@ func MakeObserverObservable(age time.Duration, length int, capacity ...int) (Obs
 		us = time.Microsecond
 	)
 
-	// Access to subscriptions
+	type subscription struct {
+		cursor    uint64
+		state     uint64    // active, canceled, closed
+		activated time.Time // track activity to deterime backoff
+	}
+
+	// cursor
 	const (
-		idle uint32 = iota
-		busy
+		maxuint64 uint64 = math.MaxUint64 // park unused cursor at maxuint64
 	)
 
-	// State of subscription and buffer
+	// state
 	const (
 		active uint64 = iota
 		canceled
@@ -44,23 +49,18 @@ func MakeObserverObservable(age time.Duration, length int, capacity ...int) (Obs
 		closed
 	)
 
-	const (
-		// Cursor is parked so it does not influence advancing the commit index.
-		parked uint64 = math.MaxUint64
-	)
-
-	type subscription struct {
-		Cursor     uint64
-		State      uint64    // active, canceled, closed
-		LastActive time.Time // track activity to deterime backoff
-	}
-
 	type subscriptions struct {
 		sync.Mutex
 		*sync.Cond
 		entries []subscription
-		access  uint32 // idle, busy
+		access  uint32 // unlocked, locked
 	}
+
+	// access
+	const (
+		unlocked uint32 = iota
+		locked
+	)
 
 	type item struct {
 		Value interface{}
@@ -68,11 +68,11 @@ func MakeObserverObservable(age time.Duration, length int, capacity ...int) (Obs
 	}
 
 	type buffer struct {
-		age time.Duration
-		len uint64
-		cap uint64
+		age  time.Duration
+		keep uint64
+		mod  uint64
+		size uint64
 
-		mod    uint64
 		items  []item
 		begin  uint64
 		end    uint64
@@ -85,25 +85,37 @@ func MakeObserverObservable(age time.Duration, length int, capacity ...int) (Obs
 	}
 
 	make := func(age time.Duration, length int, capacity ...int) *buffer {
-		cap, scap := uint64(length), uint64(32)
+		if length < 0 {
+			length = 0
+		}
+		keep := uint64(length)
+
+		cap, scap := length, 32
 		switch {
 		case len(capacity) >= 2:
-			cap, scap = uint64(capacity[0]), uint64(capacity[1])
+			cap, scap = capacity[0], capacity[1]
 		case len(capacity) == 1:
-			cap = uint64(capacity[0])
+			cap = capacity[0]
 		}
-		len := uint64(length)
-		if cap < len {
-			cap = len
+		if cap < length {
+		 	cap = length
 		}
-		cap = uint64(1) << uint(math.Ceil(math.Log2(float64(cap))))
+		if cap == 0 {
+			cap = 1
+		}
+		size := uint64(1) << uint(math.Ceil(math.Log2(float64(cap)))) // MUST(keep < size)
+
+		if scap < 1 {
+		 	scap = 1
+		}
 		buf := &buffer{
-			len:   len,
-			cap:   cap,
-			age:   age,
-			items: make([]item, cap),
-			mod:   cap - 1,
-			end:   cap,
+			age:  age,
+			keep: keep,
+			mod:  size - 1,
+			size: size,
+
+			items: make([]item, size),
+			end:   size,
 			subscriptions: subscriptions{
 				entries: make([]subscription, 0, scap),
 			},
@@ -114,38 +126,46 @@ func MakeObserverObservable(age time.Duration, length int, capacity ...int) (Obs
 	buf := make(age, length, capacity...)
 
 	accessSubscriptions := func(access func([]subscription)) bool {
-		spun := false
-		for !atomic.CompareAndSwapUint32(&buf.subscriptions.access, idle, busy) {
+		gosched := false
+		for !atomic.CompareAndSwapUint32(&buf.subscriptions.access, unlocked, locked) {
 			runtime.Gosched()
-			spun = true
+			gosched = true
 		}
 		access(buf.subscriptions.entries)
-		atomic.StoreUint32(&buf.subscriptions.access, idle)
-		return spun
+		atomic.StoreUint32(&buf.subscriptions.access, unlocked)
+		return gosched
 	}
 
 	send := func(value interface{}) {
 		for buf.commit == buf.end {
-			slowest := parked
-			spun := accessSubscriptions(func(subscriptions []subscription) {
+			full := false
+			gosched := accessSubscriptions(func(subscriptions []subscription) {
+				slowest := maxuint64
 				for i := range subscriptions {
-					cursor := atomic.LoadUint64(&subscriptions[i].Cursor)
-					if cursor < slowest {
-						slowest = cursor
+					current := atomic.LoadUint64(&subscriptions[i].cursor)
+					if current < slowest {
+						slowest = current
+					}
+				}
+				end := atomic.LoadUint64(&buf.end)
+				if atomic.LoadUint64(&buf.begin) < slowest && slowest <= end {
+					if slowest+buf.keep > end {
+						slowest = end - buf.keep + 1
+					}
+					atomic.StoreUint64(&buf.begin, slowest)
+					atomic.StoreUint64(&buf.end, slowest+buf.size)
+				} else {
+					if slowest == maxuint64 { // no subscriptions
+						atomic.AddUint64(&buf.begin, 1)
+						atomic.AddUint64(&buf.end, 1)
+					} else {
+						full = true
 					}
 				}
 			})
-			if atomic.LoadUint64(&buf.begin) < slowest && slowest <= atomic.LoadUint64(&buf.end) {
-				atomic.StoreUint64(&buf.begin, slowest)
-				atomic.StoreUint64(&buf.end, slowest+buf.mod+1)
-			} else {
-				slowest = parked
-			}
-			if slowest == parked {
-				// no subscriptions present...
-				if !spun {
-					// if we did not spin to access subscriptions, then spin now.
-					runtime.Gosched()
+			if full {
+				if !gosched {
+					runtime.Gosched() // spinlock while full
 				}
 				if atomic.LoadUint64(&buf.state) != active {
 					return // buffer no longer active
@@ -163,7 +183,7 @@ func MakeObserverObservable(age time.Duration, length int, capacity ...int) (Obs
 			if atomic.CompareAndSwapUint64(&buf.state, closing, closed) {
 				accessSubscriptions(func(subscriptions []subscription) {
 					for i := range subscriptions {
-						atomic.CompareAndSwapUint64(&subscriptions[i].State, active, closed)
+						atomic.CompareAndSwapUint64(&subscriptions[i].state, active, closed)
 					}
 				})
 			}
@@ -181,29 +201,30 @@ func MakeObserverObservable(age time.Duration, length int, capacity ...int) (Obs
 		}
 	}
 
-	appendSubscription := func(cursor uint64) (sub *subscription, err error) {
+	appendSubscription := func() (sub *subscription, err error) {
 		accessSubscriptions(func([]subscription) {
+			cursor := atomic.LoadUint64(&buf.begin)
 			s := &buf.subscriptions
 			if len(s.entries) < cap(s.entries) {
-				s.entries = append(s.entries, subscription{Cursor: cursor})
+				s.entries = append(s.entries, subscription{cursor: cursor})
 				sub = &s.entries[len(s.entries)-1]
 				return
 			}
 			for i := range s.entries {
 				sub = &s.entries[i]
-				if atomic.CompareAndSwapUint64(&sub.Cursor, parked, cursor) {
+				if atomic.CompareAndSwapUint64(&sub.cursor, maxuint64, cursor) {
 					return
 				}
 			}
-			err = ErrOutOfSubscriptions
+			sub = nil
+			err = OutOfSubscriptions
 			return
 		})
 		return
 	}
 
 	observable := func(observe Observer, subscribeOn Scheduler, subscriber Subscriber) {
-		begin := atomic.LoadUint64(&buf.begin)
-		sub, err := appendSubscription(begin)
+		sub, err := appendSubscription()
 		if err != nil {
 			runner := subscribeOn.Schedule(func() {
 				if subscriber.Subscribed() {
@@ -214,33 +235,33 @@ func MakeObserverObservable(age time.Duration, length int, capacity ...int) (Obs
 			return
 		}
 		commit := atomic.LoadUint64(&buf.commit)
-		if begin+buf.len < commit {
-			atomic.StoreUint64(&sub.Cursor, commit-buf.len)
+		if atomic.LoadUint64(&buf.begin)+buf.keep < commit {
+			atomic.StoreUint64(&sub.cursor, commit-buf.keep)
 		}
-		atomic.StoreUint64(&sub.State, atomic.LoadUint64(&buf.state))
-		sub.LastActive = time.Now()
+		atomic.StoreUint64(&sub.state, atomic.LoadUint64(&buf.state))
+		sub.activated = time.Now()
 
 		receiver := subscribeOn.ScheduleFutureRecursive(0, func(self func(time.Duration)) {
 			commit := atomic.LoadUint64(&buf.commit)
 
 			// wait for commit to move away from cursor position, indicating fresh data
-			if sub.Cursor == commit {
-				if atomic.CompareAndSwapUint64(&sub.State, canceled, canceled) {
+			if sub.cursor == commit {
+				if atomic.CompareAndSwapUint64(&sub.state, canceled, canceled) {
 					// subscription canceled
-					atomic.StoreUint64(&sub.Cursor, parked)
+					atomic.StoreUint64(&sub.cursor, maxuint64)
 					return
 				} else {
 					// subscription still active (not canceled)
 					now := time.Now()
-					if now.Before(sub.LastActive.Add(1 * ms)) {
+					if now.Before(sub.activated.Add(1 * ms)) {
 						// spinlock for 1ms (in increments of 50us) when no data from sender is arriving
 						self(50 * us) // 20kHz
 						return
-					} else if now.Before(sub.LastActive.Add(250 * ms)) {
-						if atomic.CompareAndSwapUint64(&sub.State, closed, closed) {
+					} else if now.Before(sub.activated.Add(250 * ms)) {
+						if atomic.CompareAndSwapUint64(&sub.state, closed, closed) {
 							// buffer has been closed
 							observe(nil, buf.err, true)
-							atomic.StoreUint64(&sub.Cursor, parked)
+							atomic.StoreUint64(&sub.cursor, maxuint64)
 							return
 						}
 						// spinlock between 1ms and 250ms (in increments of 500us) of no data from sender
@@ -252,7 +273,7 @@ func MakeObserverObservable(age time.Duration, length int, capacity ...int) (Obs
 							buf.subscriptions.Lock()
 							buf.subscriptions.Wait()
 							buf.subscriptions.Unlock()
-							sub.LastActive = time.Now()
+							sub.activated = time.Now()
 							self(0)
 							return
 						} else {
@@ -267,29 +288,29 @@ func MakeObserverObservable(age time.Duration, length int, capacity ...int) (Obs
 			}
 
 			// emit data and advance cursor to catch up to commit
-			if atomic.LoadUint64(&sub.State) == canceled {
-				atomic.StoreUint64(&sub.Cursor, parked)
+			if atomic.LoadUint64(&sub.state) == canceled {
+				atomic.StoreUint64(&sub.cursor, maxuint64)
 				return
 			}
-			for ; sub.Cursor != commit; atomic.AddUint64(&sub.Cursor, 1) {
-				item := &buf.items[sub.Cursor&buf.mod]
+			for ; sub.cursor != commit; atomic.AddUint64(&sub.cursor, 1) {
+				item := &buf.items[sub.cursor&buf.mod]
 				if buf.age == 0 || item.At.IsZero() || time.Since(item.At) < buf.age {
 					observe(item.Value, nil, false)
 				}
-				if atomic.LoadUint64(&sub.State) == canceled {
-					atomic.StoreUint64(&sub.Cursor, parked)
+				if atomic.LoadUint64(&sub.state) == canceled {
+					atomic.StoreUint64(&sub.cursor, maxuint64)
 					return
 				}
 			}
 
 			// all caught up; record time and loop back to wait for fresh data
-			sub.LastActive = time.Now()
+			sub.activated = time.Now()
 			self(0)
 		})
 		subscriber.OnUnsubscribe(receiver.Cancel)
 
 		subscriber.OnUnsubscribe(func() {
-			atomic.CompareAndSwapUint64(&sub.State, active, canceled)
+			atomic.CompareAndSwapUint64(&sub.state, active, canceled)
 			buf.subscriptions.Broadcast()
 		})
 	}
@@ -339,7 +360,7 @@ func (o FooObserver) Complete() {
 }
 
 //jig:template NewSubject<Foo>
-//jig:needs MakeObserverObservable, Subject<Foo>, Observer As<Foo>Observer
+//jig:needs MakeObserverObservable, Subject<Foo>, Observer As<Foo>Observer, Observable AsObservable<Foo>
 
 // NewSubjectFoo creates a new Subject. After the subject is terminated, all
 // subsequent subscriptions to the observable side will be terminated
@@ -350,7 +371,7 @@ func (o FooObserver) Complete() {
 // observable goroutine is blocked until all subscribers have processed the
 // next, error or complete notification.
 func NewSubjectFoo() SubjectFoo {
-	observer, observable := MakeObserverObservable(0, 0, 1, 16)
+	observer, observable := MakeObserverObservable(0, 0)
 	return SubjectFoo{observer.AsFooObserver(), observable.AsObservableFoo()}
 }
 
