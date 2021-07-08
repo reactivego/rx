ca9d70d427b4281d71d24b30e6e9ca12082614fc
René Post <git@xpt.nl>
2021-07-08 09:30:09 +0200

    Make Only operator appear correctly in pkg.go.dev

--- test/Only/doc.go
+++ test/Only/doc.go
@@ -1,6 +1,6 @@
 /*
-Only (e.g. OnlyInt32) filters the value stream of an observable and lets
-only the values of a specific type pass.
+Only filters the value stream of an observable and lets only the
+values of a specific type pass.
 
 So in case of OnlyInt32 it will only let int32 values pass through.
 */
