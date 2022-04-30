b16c8f2303cb059a7763458514e0a7c616466816
René Post <git@xpt.nl>
2022-04-30 15:59:40 +0200

    Refactor Create and remove CreateRecursive

--- create.go
+++ create.go
@@ -1,17 +1,24 @@
 package observable
 
-func Create[T any](create func() (T, error)) Observable[T] {
+func Create[T any](create Creator[T]) Observable[T] {
 	return func(observe Observer[T], scheduler Scheduler, subscriber Subscriber) {
-		task := func() {
+		task := func(index int, again func(int)) {
 			if subscriber.Subscribed() {
-				if next, err := create(); err == nil {
-					Of(next)(observe, scheduler, subscriber)
-				} else {
-					Throw[T](err)(observe, scheduler, subscriber)
+				next, err, done := create(index)
+				if subscriber.Subscribed() {
+					if !done {
+						observe(next, nil, false)
+						if subscriber.Subscribed() {
+							again(index + 1)
+						}
+					} else {
+						var zero T
+						observe(zero, err, true)
+					}
 				}
 			}
 		}
-		runner := scheduler.Schedule(task)
+		runner := scheduler.ScheduleLoop(0, task)
 		subscriber.OnUnsubscribe(runner.Cancel)
 	}
 }
--- creator.go
+++ creator.go
@@ -1,3 +1,3 @@
 package observable
 
-type Creator[T any] func() (Next T, Err error, Done bool)
+type Creator[T any] func(index int) (Next T, Err error, Done bool)
