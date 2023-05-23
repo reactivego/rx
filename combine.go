e96f9c2f00fe8b6d0ddbcf856c16ad44d9963c45
René Post <git@xpt.nl>
2023-05-23 11:17:52 +0200

    Fix Quadruple argument in Combine

--- combine.go
+++ combine.go
@@ -17,7 +17,7 @@ func CombineTriple[T, U, V any](first Observable[T], second Observable[U], third
 }
 
 func CombineQuadruple[T, U, V, W any](first Observable[T], second Observable[U], third Observable[V], fourth Observable[W]) Observable[Quadruple[T, U, V, W]] {
-	return Map(CombineAll(From(first.AsObservable(), second.AsObservable(), third.AsObservable())), func(next []any) Quadruple[T, U, V, W] {
+	return Map(CombineAll(From(first.AsObservable(), second.AsObservable(), third.AsObservable(), fourth.AsObservable())), func(next []any) Quadruple[T, U, V, W] {
 		return Quadruple[T, U, V, W]{First: next[0].(T), Second: next[1].(U), Third: next[2].(V), Fourth: next[3].(W)}
 	})
 }
