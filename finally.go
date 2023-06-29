2e720172cc6e309a84ed768d8104ddb0a88ec8af
René Post <git@xpt.nl>
2023-06-29 17:29:41 +0200

    Pass error as argument to callback

--- finally.go
+++ finally.go
@@ -1,10 +1,10 @@
 package x
 
-func (observable Observable[T]) Finally(f func()) Observable[T] {
+func (observable Observable[T]) Finally(f func(error)) Observable[T] {
 	return func(observe Observer[T], scheduler Scheduler, subscriber Subscriber) {
 		observer := func(next T, err error, done bool) {
 			if done {
-				f()
+				f(err)
 			}
 			observe(next, err, done)
 		}
