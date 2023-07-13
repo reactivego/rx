7d95d4218335578f5c100834750ac820fcd6be0f
René Post <git@xpt.nl>
2023-07-13 11:34:17 +0200

    Remove Errorf and Errorw funcs

--- error.go
+++ error.go
@@ -1,15 +1,5 @@
 package x
 
-import "fmt"
-
 type Error string
 
 func (e Error) Error() string { return string(e) }
-
-func Errorf(format string, items ...any) error {
-	return Error(fmt.Sprintf(format, items...))
-}
-
-func Errorw(err error) error {
-	return Error(fmt.Sprintf("%[1]T:%[1]v", err))
-}
