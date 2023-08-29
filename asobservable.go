712ddc3ccd8d2aa62583a602aac16367284ac753
René Post <git@xpt.nl>
2023-08-29 12:50:40 +0200

    Add AsObserver and refactor AsObservable

--- asobservable.go
+++ asobservable.go
@@ -1,31 +1,13 @@
 package x
 
-const TypecastFailed = Error("typecast failed")
-
 func AsObservable[T any](observable Observable[any]) Observable[T] {
 	return func(observe Observer[T], scheduler Scheduler, subscriber Subscriber) {
-		observer := func(next any, err error, done bool) {
-			if !done {
-				if nextT, ok := next.(T); ok {
-					observe(nextT, err, done)
-				} else {
-					var zero T
-					observe(zero, TypecastFailed, true)
-				}
-			} else {
-				var zero T
-				observe(zero, err, true)
-			}
-		}
-		observable(observer, scheduler, subscriber)
+		observable(observe.AsObserver(), scheduler, subscriber)
 	}
 }
 
 func (observable Observable[T]) AsObservable() Observable[any] {
 	return func(observe Observer[any], scheduler Scheduler, subscriber Subscriber) {
-		observer := func(next T, err error, done bool) {
-			observe(next, err, done)
-		}
-		observable(observer, scheduler, subscriber)
+		observable(AsObserver[T](observe), scheduler, subscriber)
 	}
 }
---
+++ asobserver.go
@@ -0,0 +1,25 @@
+package x
+
+const TypecastFailed = Error("typecast failed")
+
+func AsObserver[T any](observe Observer[any]) Observer[T] {
+	return func(next T, err error, done bool) {
+		observe(next, err, done)
+	}
+}
+
+func (observe Observer[T]) AsObserver() Observer[any] {
+	return func(next any, err error, done bool) {
+		if !done {
+			if nextT, ok := next.(T); ok {
+				observe(nextT, err, done)
+			} else {
+				var zero T
+				observe(zero, TypecastFailed, true)
+			}
+		} else {
+			var zero T
+			observe(zero, err, true)
+		}
+	}
+}
