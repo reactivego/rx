261c14122d8e89e7b2d38641e3414165992cd446
René Post <git@xpt.nl>
2025-03-31 10:36:28 +0200

    Remove Observable.Reduce method

--- reduce.go
+++ reduce.go
@@ -17,21 +17,3 @@ func Reduce[T, U any](observable Observable[T], seed U, accumulator func(acc U, 
 		observable(observer, scheduler, subscriber)
 	}
 }
-
-func (observable Observable[T]) Reduce(seed T, accumulator func(acc, next T) T) Observable[T] {
-	return func(observe Observer[T], scheduler Scheduler, subscriber Subscriber) {
-		state := seed
-		observer := func(next T, err error, done bool) {
-			switch {
-			case !done:
-				state = accumulator(state, next)
-			case err != nil:
-				var zero T
-				observe(zero, err, true)
-			default:
-				Of(state)(observe, scheduler, subscriber)
-			}
-		}
-		observable(observer, scheduler, subscriber)
-	}
-}
