644e7a3491cb5336bc256d202b221f362a6ef98c
René Post <git@xpt.nl>
2025-03-31 18:10:38 +0200

    Rename observables to sources in WithLatestFromAll

--- withlatestfromall.go
+++ withlatestfromall.go
@@ -6,7 +6,7 @@ import (
 
 func WithLatestFromAll[T any](observable Observable[Observable[T]]) Observable[[]T] {
 	return func(observe Observer[[]T], scheduler Scheduler, subscriber Subscriber) {
-		observables := []Observable[T](nil)
+		sources := []Observable[T](nil)
 		var observers struct {
 			sync.Mutex
 			assigned    []bool
@@ -14,19 +14,19 @@ func WithLatestFromAll[T any](observable Observable[Observable[T]]) Observable[[
 			initialized int
 			done        bool
 		}
-		makeObserver := func(index int) Observer[T] {
+		makeObserver := func(sourceIndex int) Observer[T] {
 			observer := func(next T, err error, done bool) {
 				observers.Lock()
 				defer observers.Unlock()
 				if !observers.done {
 					switch {
 					case !done:
-						if !observers.assigned[index] {
-							observers.assigned[index] = true
+						if !observers.assigned[sourceIndex] {
+							observers.assigned[sourceIndex] = true
 							observers.initialized++
 						}
-						observers.values[index] = next
-						if index == 0 && observers.initialized == len(observers.values) {
+						observers.values[sourceIndex] = next
+						if sourceIndex == 0 && observers.initialized == len(observers.values) {
 							observe(observers.values, nil, false)
 						}
 					case err != nil:
@@ -34,7 +34,7 @@ func WithLatestFromAll[T any](observable Observable[Observable[T]]) Observable[[
 						var zero []T
 						observe(zero, err, true)
 					default:
-						if index == 0 {
+						if sourceIndex == 0 {
 							observers.done = true
 							var zero []T
 							observe(zero, nil, true)
@@ -48,21 +48,21 @@ func WithLatestFromAll[T any](observable Observable[Observable[T]]) Observable[[
 		observer := func(next Observable[T], err error, done bool) {
 			switch {
 			case !done:
-				observables = append(observables, next)
+				sources = append(sources, next)
 			case err != nil:
 				var zero []T
 				observe(zero, err, true)
 			default:
 				runner := scheduler.Schedule(func() {
 					if subscriber.Subscribed() {
-						numObservables := len(observables)
-						observers.assigned = make([]bool, numObservables)
-						observers.values = make([]T, numObservables)
-						for i, v := range observables {
+						numSources := len(sources)
+						observers.assigned = make([]bool, numSources)
+						observers.values = make([]T, numSources)
+						for sourceIndex, source := range sources {
 							if !subscriber.Subscribed() {
 								return
 							}
-							v.AutoUnsubscribe()(makeObserver(i), scheduler, subscriber)
+							source.AutoUnsubscribe()(makeObserver(sourceIndex), scheduler, subscriber)
 						}
 					}
 				})
