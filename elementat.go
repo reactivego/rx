9bc7db2a4ca1fa38b5c7b4669042d74a4ddae804
René Post <git@xpt.nl>
2025-03-31 11:25:56 +0200

    Refactor ElementAt to use pipe operator

--- elementat.go
+++ elementat.go
@@ -1,23 +1,29 @@
 package rx
 
-func (observable Observable[T]) ElementAt(n int) Observable[T] {
-	return func(observe Observer[T], scheduler Scheduler, subscriber Subscriber) {
-		i := 0
-		observer := func(next T, err error, done bool) {
-			if !done {
-				if i >= n {
-					if i == n {
-						observe(next, nil, false)
-					} else {
-						var zero T
-						observe(zero, nil, true)
+func ElementAt[T any](n int) Pipe[T] {
+	return func(observable Observable[T]) Observable[T] {
+		return func(observe Observer[T], scheduler Scheduler, subscriber Subscriber) {
+			i := 0
+			observer := func(next T, err error, done bool) {
+				if !done {
+					if i >= n {
+						if i == n {
+							observe(next, nil, false)
+						} else {
+							var zero T
+							observe(zero, nil, true)
+						}
 					}
+					i++
+				} else {
+					observe(next, err, done)
 				}
-				i++
-			} else {
-				observe(next, err, done)
 			}
+			observable(observer, scheduler, subscriber)
 		}
-		observable(observer, scheduler, subscriber)
 	}
+}
+
+func (observable Observable[T]) ElementAt(n int) Observable[T] {
+	return ElementAt[T](n)(observable)
 }
