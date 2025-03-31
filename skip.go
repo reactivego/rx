a1d78ebfebe440dc19b582ad590e6362301e2fb7
René Post <git@xpt.nl>
2025-03-31 09:14:49 +0200

    Remove unused aSkip function

--- skip.go
+++ skip.go
@@ -1,17 +1,5 @@
 package rx
 
-func (observable Observable[T]) aSkip(n int) Observable[T] {
-	return func(observe Observer[T], scheduler Scheduler, subscriber Subscriber) {
-		i := 0
-		observable(func(next T, err error, done bool) {
-			if done || i >= n {
-				observe(next, err, done)
-			}
-			i++
-		}, scheduler, subscriber)
-	}
-}
-
 func Skip[T any](n int) Pipe[T] {
 	return func(observable Observable[T]) Observable[T] {
 		return func(observe Observer[T], scheduler Scheduler, subscriber Subscriber) {
