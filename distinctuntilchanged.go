ed1998b929efd0aab4b89f90e2a92f59e185af9d
René Post <git@xpt.nl>
2025-04-06 11:24:01 +0200

    Refactor DistinctUntilChanged as a pipe function

--- distinctuntilchanged.go
+++ distinctuntilchanged.go
@@ -1,26 +1,28 @@
 package rx
 
-func DistinctUntilChanged[T comparable](observable Observable[T]) Observable[T] {
-	return observable.DistinctUntilChanged(Equal[T]())
+func DistinctUntilChanged[T any](equal func(T, T) bool) Pipe[T] {
+	return func(observable Observable[T]) Observable[T] {
+		return func(observe Observer[T], subscribeOn Scheduler, subscriber Subscriber) {
+			var seen struct {
+				initialized bool
+				value       T
+			}
+			observer := func(next T, err error, done bool) {
+				if !done {
+					if seen.initialized && equal(seen.value, next) {
+						return // skip equal
+					} else {
+						seen.initialized = true
+						seen.value = next
+					}
+				}
+				observe(next, err, done)
+			}
+			observable(observer, subscribeOn, subscriber)
+		}
+	}
 }
 
 func (observable Observable[T]) DistinctUntilChanged(equal func(T, T) bool) Observable[T] {
-	return func(observe Observer[T], subscribeOn Scheduler, subscriber Subscriber) {
-		var seen struct {
-			initialized bool
-			value       T
-		}
-		observer := func(next T, err error, done bool) {
-			if !done {
-				if seen.initialized && equal(seen.value, next) {
-					return // skip equal
-				} else {
-					seen.initialized = true
-					seen.value = next
-				}
-			}
-			observe(next, err, done)
-		}
-		observable(observer, subscribeOn, subscriber)
-	}
+	return DistinctUntilChanged(equal)(observable)
 }
