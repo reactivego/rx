8dc543190660ba1cea9717bfbde997dd4748e75e
René Post <git@xpt.nl>
2025-04-09 13:07:36 +0200

    Add documentation for Go and Ignore functions

--- go.go
+++ go.go
@@ -1,5 +1,8 @@
 package rx
 
+// Go subscribes to the observable on a new goroutine, ignoring all emissions.
+// This method provides a convenient way to start consuming the observable asynchronously
+// without processing its values. Returns a Subscription which can be used to unsubscribe.
 func (observable Observable[T]) Go() Subscription {
 	return observable.Subscribe(Ignore[T](), Goroutine)
 }
--- ignore.go
+++ ignore.go
@@ -1,5 +1,8 @@
 package rx
 
+// Ignore creates an Observer that simply discards any emissions
+// from an Observable. It is useful when you need to create an
+// Observer but don't care about its values.
 func Ignore[T any]() Observer[T] {
 	return func(next T, err error, done bool) {}
 }
