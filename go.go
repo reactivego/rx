111f1052ca63f7bb3caff382eadd340abc4787a8
René Post <git@xpt.nl>
2025-04-09 13:00:58 +0200

    Simplify Go() method by removing scheduler parameter

--- go.go
+++ go.go
@@ -1,9 +1,5 @@
 package rx
 
-func (observable Observable[T]) Go(schedulers ...Scheduler) Subscription {
-	if len(schedulers) == 0 {
-		return observable.Subscribe(Ignore[T](), Goroutine)
-	} else {
-		return observable.Subscribe(Ignore[T](), schedulers[0])
-	}
+func (observable Observable[T]) Go() Subscription {
+	return observable.Subscribe(Ignore[T](), Goroutine)
 }
