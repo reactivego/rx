1c239fb76601dccb27138075963c9fe3326ebce5
René Post <git@xpt.nl>
2025-04-10 14:52:19 +0200

    Add optional scheduler parameter to Observable.Go()

--- go.go
+++ go.go
@@ -1,8 +1,14 @@
 package rx
 
-// Go subscribes to the observable on a new goroutine, ignoring all emissions.
-// This method provides a convenient way to start consuming the observable asynchronously
-// without processing its values. Returns a Subscription which can be used to unsubscribe.
-func (observable Observable[T]) Go() Subscription {
-	return observable.Subscribe(Ignore[T](), Goroutine)
+// Go subscribes to the observable and starts execution on a separate goroutine.
+// It ignores all emissions from the observable sequence, making it useful when you
+// only care about side effects and not the actual values. By default, it uses the
+// Goroutine scheduler, but an optional scheduler can be provided. Returns a
+// Subscription that can be used to cancel the subscription when no longer needed.
+func (observable Observable[T]) Go(schedulers ...Scheduler) Subscription {
+	if len(schedulers) == 0 {
+		return observable.Subscribe(Ignore[T](), Goroutine)
+	} else {
+		return observable.Subscribe(Ignore[T](), schedulers[0])
+	}
 }
