70c4eadbbc53f63a6af90636870c73fb3c31ea29
René Post <git@xpt.nl>
2025-04-29 13:32:12 +0200

    Add Retry function as an operator pipe

--- retry.go
+++ retry.go
@@ -1,10 +1,14 @@
 package rx
 
-import (
-	"time"
-)
+import "time"
+
+func Retry[T any](limit ...int) Pipe[T] {
+	return func(observable Observable[T]) Observable[T] {
+		backoff := func(int) time.Duration { return 1 * time.Millisecond }
+		return observable.RetryTime(backoff, limit...)
+	}
+}
 
 func (observable Observable[T]) Retry(limit ...int) Observable[T] {
-	backoff := func(int) time.Duration { return 1 * time.Millisecond }
-	return observable.RetryTime(backoff, limit...)
+	return Retry[T](limit...)(observable)
 }
