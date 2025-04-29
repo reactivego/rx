c61bfc8e05ceeaf045f7890351a7e3fd0466647c
René Post <git@xpt.nl>
2025-04-29 12:03:24 +0200

    Refactor AutoUnsubscribe to follow pipe pattern

--- autounsubscribe.go
+++ autounsubscribe.go
@@ -1,14 +1,20 @@
 package rx
 
-func (observable Observable[T]) AutoUnsubscribe() Observable[T] {
-	return func(observe Observer[T], scheduler Scheduler, subscriber Subscriber) {
-		subscriber = subscriber.Add()
-		observer := func(next T, err error, done bool) {
-			observe(next, err, done)
-			if done {
-				subscriber.Unsubscribe()
+func AutoUnsubscribe[T any]() Pipe[T] {
+	return func(observable Observable[T]) Observable[T] {
+		return func(observe Observer[T], scheduler Scheduler, subscriber Subscriber) {
+			subscriber = subscriber.Add()
+			observer := func(next T, err error, done bool) {
+				observe(next, err, done)
+				if done {
+					subscriber.Unsubscribe()
+				}
 			}
+			observable(observer, scheduler, subscriber)
 		}
-		observable(observer, scheduler, subscriber)
 	}
+}
+
+func (observable Observable[T]) AutoUnsubscribe() Observable[T] {
+	return AutoUnsubscribe[T]()(observable)
 }
