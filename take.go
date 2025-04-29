81bf733a0efd9d835b8a4693ea097fb8a41d8032
René Post <git@xpt.nl>
2025-04-29 08:31:15 +0200

    Enhance Take operator with better edge case handling
    Fixes #3

--- take.go
+++ take.go
@@ -1,22 +1,35 @@
 package rx
 
+// Take returns an Observable that emits only the first count values emitted by
+// the source Observable. If the source emits fewer than count values then all
+// of its values are emitted. After that, it completes, regardless if the source
+// completes.
 func Take[T any](n int) Pipe[T] {
 	return func(observable Observable[T]) Observable[T] {
-		return func(observe Observer[T], scheduler Scheduler, subscriber Subscriber) {
+		if n <= 0 {
+			return Empty[T]()
+		}
+		return Observable[T](func(observe Observer[T], scheduler Scheduler, subscriber Subscriber) {
 			taken := 0
 			observable(func(next T, err error, done bool) {
-				if done || taken < n {
-					observe(next, err, done)
+				if !done {
+					observe(next, nil, false)
+					if taken++; taken == n {
+						var zero T
+						observe(zero, nil, true)
+					}
 				} else {
-					var zero T
-					observe(zero, nil, true)
+					observe(next, err, true)
 				}
-				taken++
 			}, scheduler, subscriber)
-		}
+		}).AutoUnsubscribe()
 	}
 }
 
+// Take returns an Observable that emits only the first count values emitted by
+// the source Observable. If the source emits fewer than count values then all
+// of its values are emitted. After that, it completes, regardless if the source
+// completes.
 func (observable Observable[T]) Take(n int) Observable[T] {
 	return Take[T](n)(observable)
 }
