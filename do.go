7e4105d68880d1ed3cd29a6e091d86a664f1bde4
René Post <git@xpt.nl>
2025-04-29 09:12:04 +0200

    Refactor Do and Finally to use Tap with observers

--- do.go
+++ do.go
@@ -1,18 +1,11 @@
 package rx
 
+// Do is a shortcut for Tap(OnNext(f))
 func Do[T any](f func(T)) Pipe[T] {
-	return func(observable Observable[T]) Observable[T] {
-		return func(observe Observer[T], scheduler Scheduler, subscriber Subscriber) {
-			observable(func(next T, err error, done bool) {
-				if !done {
-					f(next)
-				}
-				observe(next, err, done)
-			}, scheduler, subscriber)
-		}
-	}
+	return Tap(OnNext(f))
 }
 
+// Do is a shortcut for Tap(OnNext(f))
 func (observable Observable[T]) Do(f func(next T)) Observable[T] {
-	return Do[T](f)(observable)
+	return Do(f)(observable)
 }
--- finally.go
+++ finally.go
@@ -1,18 +1,11 @@
 package rx
 
+// Finally is  shortcut for Tap(OnDone(f))
 func Finally[T any](f func(error)) Pipe[T] {
-	return func(observable Observable[T]) Observable[T] {
-		return func(observe Observer[T], scheduler Scheduler, subscriber Subscriber) {
-			observable(func(next T, err error, done bool) {
-				if done {
-					f(err)
-				}
-				observe(next, err, done)
-			}, scheduler, subscriber)
-		}
-	}
+	return Tap(OnDone[T](f))
 }
 
+// Finally is  shortcut for Tap(OnDone(f))
 func (observable Observable[T]) Finally(f func(error)) Observable[T] {
 	return Finally[T](f)(observable)
 }
