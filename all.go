64885809db62c8d75231bb5bc5b8ec3b5b42e12c
René Post <git@xpt.nl>
2025-04-29 13:10:21 +0200

    Add All2 function and rename yielder variable to yielding

--- all.go
+++ all.go
@@ -2,14 +2,24 @@ package rx
 
 import "iter"
 
+func All2[T, U any](observable Observable[Tuple2[T, U]], scheduler ...Scheduler) iter.Seq2[T, U] {
+	return func(yield func(T, U) bool) {
+		yielding := func(next Tuple2[T, U]) bool {
+			return yield(next.First, next.Second)
+		}
+		err := observable.TakeWhile(yielding).Wait(scheduler...)
+		_ = err // ignores error! so will fail silently
+	}
+}
+
 func (observable Observable[T]) All(scheduler ...Scheduler) iter.Seq2[int, T] {
 	return func(yield func(int, T) bool) {
 		index := -1
-		yielder := func(value T) bool {
+		yielding := func(value T) bool {
 			index++
 			return yield(index, value)
 		}
-		err := observable.TakeWhile(yielder).Wait(scheduler...)
+		err := observable.TakeWhile(yielding).Wait(scheduler...)
 		_ = err // ignores error! so will fail silently
 	}
 }
