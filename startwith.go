4db454af7f5d1bf3373745e70902f061a9cc6e1e
René Post <git@xpt.nl>
2025-04-29 13:17:07 +0200

    Add StartWith pipe function and refactor method to use it

--- startwith.go
+++ startwith.go
@@ -1,5 +1,11 @@
 package rx
 
+func StartWith[T any](values ...T) Pipe[T] {
+	return func(observable Observable[T]) Observable[T] {
+		return From(values...).ConcatWith(observable)
+	}
+}
+
 func (observable Observable[T]) StartWith(values ...T) Observable[T] {
-	return From(values...).ConcatWith(observable)
+	return StartWith(values...)(observable)
 }
