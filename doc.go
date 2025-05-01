e0eac0302c4e2aed2d168cbb853a6a3c292c2aab
René Post <git@xpt.nl>
2025-05-01 11:46:26 +0200

    Add package level documentation for rx package

---
+++ doc.go
@@ -0,0 +1,3 @@
+// Package rx provides Reactive Extensions, a powerful API for asynchronous programming in Go,
+// built around observables and operators to process streams of data seamlessly.
+package rx
